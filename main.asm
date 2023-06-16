882cf21d6b489abfe16b439aee2851835018acd7
Hafsa9876 <2018n05944@gmail.com>
2023-06-16 10:17:17 +0500

    Print_rect

--- main.asm
+++ main.asm
@@ -334,7 +334,36 @@ clrscr:
 		loop lop1
 		pop cx
 		ret
-		
+				
+		printstr: push bp
+		 mov bp, sp
+		 push es
+		 push ax
+		 push cx
+		 push si
+		 push di
+		 mov ax, 0xb800
+		 mov es, ax ; point es to video base
+		 mov al, 80 ; load al with columns per row
+		 mul byte [bp+10] ; multiply with y position
+		 add ax, [bp+12] ; add x position
+		 shl ax, 1 ; turn into byte offset
+		 mov di,ax ; point di to required location
+		 mov si, [bp+6] ; point si to string
+		 mov cx, [bp+4] ; load length of string in cx
+		 mov ah, [bp+8] ; load attribute in ah
+		nextchar1: mov al, [si] ; load next char of string
+		 mov [es:di], ax ; show this char on screen
+		 add di, 2 ; move to next screen location
+		 add si, 1 ; move to next char in string
+		 loop nextchar1 ; repeat the operation cx times
+		 pop di
+		 pop si
+		 pop cx
+		 pop ax
+		 pop es
+		 pop bp
+		 ret 10 
 		printnum_m: push bp 
 		 mov bp, sp
 		 push es
