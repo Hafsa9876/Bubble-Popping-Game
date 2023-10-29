07415cb0ba39c7c8800a959fc1ea2d2fb86e6e47
Hafsa Najeeb <115767193+Hafsa9876@users.noreply.github.com>
2023-10-29 18:19:30 +0500

    Update main.asm

--- main.asm
+++ main.asm
@@ -1243,6 +1243,28 @@ game_ending:
 		 push ax ; push address of message
 		push  word [len_over];; push message length
 		 call printstr 
+
+				 mov ax, 10
+		 push ax ; push x position
+		 mov ax, 15
+		 push ax ; push y position
+		 mov ax,  00111111b; blue on black attribute
+		 push ax ; push attribute
+		 mov ax, e12
+		 push ax ; push address of message
+		push  word [len_over];; push message length
+		 call printstr
+		 
+		  mov ax, 10
+		 push ax ; push x position
+		 mov ax, 16
+		 push ax ; push y position
+		 mov ax,  00111111b; blue on black attribute
+		 push ax ; push attribute
+		 mov ax, e13
+		 push ax ; push address of message
+		push  word [len_over];; push message length
+		 call printstr
 		 
 		ret
 start:
