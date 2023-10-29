6d80442c6ab6ce49379bad9a26928e8fc4f49411
Hafsa Najeeb <115767193+Hafsa9876@users.noreply.github.com>
2023-10-29 18:24:23 +0500

    Update main.asm

--- main.asm
+++ main.asm
@@ -41,20 +41,86 @@ comp:dw 0
 old_kbisr: dd 0
 temp:dw 0
 
+
 	; subroutine to clear the screen
 	sleep: push cx
 	mov cx, 0xFFFF
 	delay: loop delay
 	pop cx
 	ret
-		sec_delay:
+	
+	add_score:
+		 push cx
+		 mov cx,0
+		 add cx,10
+		 pop cx
+		   ret
+		   
+	
+	
+clrscr:
+
+	push es
+	push di
+	push ax
+
+	mov ax,0xb800
+	mov es,ax
+	mov di,0
+
+	nextchar:
+			mov word[es:di],0x0720
+			add di,2
+			cmp di,4000
+			jne nextchar
+
+	pop ax
+	pop di
+	pop es
+	ret
+			printnum: push bp                        ;printing score
+		 mov bp, sp
+		 push es
+		 push ax
+		 push bx
+		 push cx
+		 push dx
+		 push di
+		 mov ax, 0xb800
+		 mov es, ax ; point es to video base
+		 mov ax, [bp+4] ; load number in ax
+		 mov bx, 10 ; use base 10 for division
+		 mov cx, 0 ; initialize count of digits
+		nextdigit: mov dx, 0 ; zero upper half of dividend
+		 div bx ; divide by 10
+		 add dl, 0x30 ; convert digit into ascii value
+		 push dx ; save ascii value on stack
+		 inc cx ; increment count of values
+		 cmp ax, 0 ; is the quotient zero
+		 jnz nextdigit ; if no divide it again
+		 mov di, 2262 ; point di to top left column
+		 nextpos: pop dx ; remove a digit from the stack
+		 mov dh, 00001011b ; use normal attribute
+		 mov [es:di], dx ; print char on screen
+		 add di, 2 ; move to next screen location
+		 loop nextpos ; repeat for all digits on stack
+		 pop di
+		 pop dx
+		 pop cx
+		 pop bx
+		 pop ax
+		 pop es
+		 pop bp
+		 ret 2 
+	
+	sec_delay:
     MOV     CX, 07H
 	MOV     DX, 0xa120
 	MOV     AH, 86H
 	INT     15H
 	ret
 
-beep:
+	beep:
 
      push ax
       push bx
@@ -92,16 +158,8 @@ beep:
       pop ax
 
       ret
-
-
-	add_score:
-		 push cx
-		 mov cx,0
-		 add cx,10
-		 pop cx
-		   ret
-		   
-		t1:
+	
+	t1:
 	add word[Time_s2],1
 	call sec_delay
 	mov ax,[Time_s2]
@@ -115,7 +173,6 @@ beep:
 	push ax
 	call printnum_s1
 	ret
-
 	
 	t3:add word[Time_m],1
 	call sec_delay
@@ -131,6 +188,7 @@ beep:
 	push ax
 	call printnum_s2
 	ret
+	
 	update_time:
 	push ax
 	push cx
@@ -142,228 +200,46 @@ beep:
 	cmp word[Time_s2],9
 	jge t_l2
 	
-
-
-
-clrscr:
-
-	push es
-	push di
-	push ax
-
-	mov ax,0xb800
-	mov es,ax
-	mov di,0
-
-	nextchar:
-			mov word[es:di],0x0720
-			add di,2
-			cmp di,4000
-			jne nextchar
-
+	
+	t_l1:call t1
+	jmp m_end
+	
+	
+	t_l2:cmp word[Time_s2],59
+	jge t_l3
+	call t2
+	jmp m_end
+	
+	t_l3:call t3
+	jmp m_end
+	m2:
+	cmp word[Time_s2],9
+	jge t_l22
+	
+		
+	t_l11:call t1
+	jmp m_end
+	
+	
+	t_l22:cmp word[Time_s2],59
+	jge t_l33
+	call t2
+	jmp m_end
+	
+	t_l33:call t3
+	mov byte[cs:temp],1
+	jmp m_end
+	
+	
+m_end:
+	pop si
+	pop cx
 	pop ax
-	pop di
-	pop es
 	ret
-
 	
-		
-		scrollup: push bp
-		 mov bp,sp
-		 push ax
-		 push cx
-		 push si
-		 push di
-		 push es
-		 push ds
-		 mov ax, 80 ; load chars per row in ax
-		 mul byte [bp+4] ; calculate source position
-		 mov si, ax ; load source position in si
-		 push si ; save position for later use
-		 shl si, 1 ; convert to byte offset
-		 mov cx, 2000 ; number of screen locations
-		 sub cx, ax ; count of words to move
-		 mov ax, 0xb800
-		 mov es, ax ; point es to video base
-		 mov ds, ax ; point ds to video base
-		 xor di, di ; point di to top left column
-		 cld ; set auto increment mode
-		 rep movsw ; scroll up
-
-		 mov ax, 0x0720 ; space in normal attribute
-		 pop cx ; count of positions to clear
-		 rep stosw ; clear the scrolled space
-		 pop ds
-		 pop es
-		 pop di
-		 pop si
-		 pop cx
-		 pop ax
-		 pop bp
-		 ret 2 
-		 
-		 
-
-		printrec:
-		push ax
-		push bx
-		push cx
-		push dx
-		mov ah,06h
-		mov al,3
-		mov bh,byte[colour]
-		mov ch,byte[top_row]  ;top left row
-		mov cl,byte[top_col]  ;top left col
-		mov dh,25;bottom right row
-		mov dl,byte[bot_col]  ;bottom right col
-		int 10h
-		mov cx,14
-	l1:mov ax,1
-		push ax 
-		call scrollup
-		;mnaging speed
-		call sleep
-		call sleep
-		call sleep
-		call sleep
-		call sleep
-		call sleep
-		call sleep
-		call sleep
-		call sleep
-		call sleep
-		call sleep
-		call sleep
-		sub cx,1
-		loop l1
-		pop dx
-		pop cx
-		pop bx
-		pop ax
-		ret
-		
-		
-		
-	print_sc:
-		;1st
-		mov byte[top_col],34
-		mov byte[top_row],10	
-		mov byte[bot_col],39
-		mov byte[colour],15h
-		call printrec	
-
-		;2nd
-		mov byte[top_col],21
-		mov byte[top_row],10	
-		mov byte[bot_col],26
-		mov byte[colour],33h
-		call printrec	
-		
-		;3rth
-		mov byte[top_col],60
-		mov byte[top_row],20
-		mov byte[bot_col],65
-		mov byte[colour],64h
-		call printrec
-		;4th
-		mov byte[top_col],7
-		mov byte[top_row],7	
-		mov byte[bot_col],12
-		mov byte[colour],22h
-		call printrec	
-		;5th
-		mov byte[top_col],43
-		mov byte[top_row],7	
-		mov byte[bot_col],48
-		mov byte[colour],44h
-		call printrec
-		;6th
-		mov byte[top_col],23
-		mov byte[top_row],7	
-		mov byte[bot_col],28
-		mov byte[colour],51h
-		call printrec
-		;7th
-		mov byte[top_col],50
-		mov byte[top_row],7	
-		mov byte[bot_col],55
-		mov byte[colour],33h
-		call printrec
 	
-		ret
 	
-			 printnum_s1: push bp 
-		 mov bp, sp
-		 push es
-		 push ax
-		 push bx
-		 push cx
-		 push dx
-		 push di
-		 mov ax, 0xb800
-		 mov es, ax ; point es to video base
-		 mov ax, [bp+4] ; load number in ax
-		 mov bx, 10 ; use base 10 for division
-		 mov cx, 0 ; initialize count of digits
-		nextdigitps1: mov dx, 0 ; zero upper half of dividend
-		 div bx ; divide by 10
-		 add dl, 0x30 ; convert digit into ascii value
-		 push dx ; save ascii value on stack
-		 inc cx ; increment count of values
-		 cmp ax, 0 ; is the quotient zero
-		 jnz nextdigitps1 ; if no divide it again
-		 mov di, 984 ; point di to top left column
-		 nextposps1: pop dx ; remove a digit from the stack
-		 mov dh, 00001011b ; use normal attribute
-		 mov [es:di], dx ; print char on screen
-		 add di, 2 ; move to next screen location
-		 loop nextposps1 ; repeat for all digits on stack
-		 pop di
-		 pop dx
-		 pop cx
-		 pop bx
-		 pop ax
-		 pop es
-		 pop bp
-		 ret 2 	
-		main_print:
-		push cx
-		mov cx,6
-		lop1:call print_sc
-		sub cx,1
-		loop lop1
-		pop cx
-		ret
-				
-		printstr: push bp
-		 mov bp, sp
-		 push es
-		 push ax
-		 push cx
-		 push si
-		 push di
-		 mov ax, 0xb800
-		 mov es, ax ; point es to video base
-		 mov al, 80 ; load al with columns per row
-		 mul byte [bp+10] ; multiply with y position
-		 add ax, [bp+12] ; add x position
-		 shl ax, 1 ; turn into byte offset
-		 mov di,ax ; point di to required location
-		 mov si, [bp+6] ; point si to string
-		 mov cx, [bp+4] ; load length of string in cx
-		 mov ah, [bp+8] ; load attribute in ah
-		nextchar1: mov al, [si] ; load next char of string
-		 mov [es:di], ax ; show this char on screen
-		 add di, 2 ; move to next screen location
-		 add si, 1 ; move to next char in string
-		 loop nextchar1 ; repeat the operation cx times
-		 pop di
-		 pop si
-		 pop cx
-		 pop ax
-		 pop es
-		 pop bp
-		 ret 10 
+	
 		printnum_m: push bp 
 		 mov bp, sp
 		 push es
@@ -398,7 +274,42 @@ clrscr:
 		 pop es
 		 pop bp
 		 ret 2 
- printnum_s2: push bp 
+		 printnum_s1: push bp 
+		 mov bp, sp
+		 push es
+		 push ax
+		 push bx
+		 push cx
+		 push dx
+		 push di
+		 mov ax, 0xb800
+		 mov es, ax ; point es to video base
+		 mov ax, [bp+4] ; load number in ax
+		 mov bx, 10 ; use base 10 for division
+		 mov cx, 0 ; initialize count of digits
+		nextdigitps1: mov dx, 0 ; zero upper half of dividend
+		 div bx ; divide by 10
+		 add dl, 0x30 ; convert digit into ascii value
+		 push dx ; save ascii value on stack
+		 inc cx ; increment count of values
+		 cmp ax, 0 ; is the quotient zero
+		 jnz nextdigitps1 ; if no divide it again
+		 mov di, 984 ; point di to top left column
+		 nextposps1: pop dx ; remove a digit from the stack
+		 mov dh, 00001011b ; use normal attribute
+		 mov [es:di], dx ; print char on screen
+		 add di, 2 ; move to next screen location
+		 loop nextposps1 ; repeat for all digits on stack
+		 pop di
+		 pop dx
+		 pop cx
+		 pop bx
+		 pop ax
+		 pop es
+		 pop bp
+		 ret 2 
+		 
+		 printnum_s2: push bp 
 		 mov bp, sp
 		 push es
 		 push ax
@@ -467,60 +378,10 @@ clrscr:
 		 pop es
 		 pop bp
 		 ret 2 
-		cmp_s:
-		mov ch,0  ;row
-			l02:mov cl,20 ;col
-				l3:mov dh, ch
-					mov dl, cl
-					mov bh, 0
-					mov ah, 2
-					int 10h
-					mov bh,0
-					mov ah,08h
-					int 10h
-					cmp al,'s'
-					je ptnt_s
-					add cl,1
-					cmp cl,78
-					jne l3
-				add ch,1
-				cmp ch,24
-				jne l02
-			jmp s_end
-				ptnt_s:		
-				add word[score],10
-				
-				 mov ax,[score]
-				 push ax
-				 call printnum
-		      call beep				
-				mov byte[row_index],ch
-				mov byte[col_index],cl
-				sub byte[col_index],3
-				sub byte[row_index],1
-				mov si,0
-				j2:mov dh, byte[row_index]
-				mov dl, byte[col_index]
-				mov bh, 0
-				mov ah, 2
-				int 10h
-				
-				mov al,'*'
-				mov bh,0
-				mov bl,10001011b
-				mov cx,6
-				mov ah,09h
-				int 10h
-				add byte[row_index],1
-				add si,1
-				cmp si,3
-				jne j2
-				
-				
-				s_end:
-				ret
-					
-scrollup: push ax
+	
+	
+		
+		scrollup: push ax
 		push bx
 		push cx
 		push dx
@@ -590,7 +451,116 @@ scrollup: push ax
 		
 		
 		a_end:
-		ret	
+		ret
+		
+		cmp_s:
+		mov ch,0  ;row
+			l02:mov cl,20 ;col
+				l3:mov dh, ch
+					mov dl, cl
+					mov bh, 0
+					mov ah, 2
+					int 10h
+					mov bh,0
+					mov ah,08h
+					int 10h
+					cmp al,'s'
+					je ptnt_s
+					add cl,1
+					cmp cl,78
+					jne l3
+				add ch,1
+				cmp ch,24
+				jne l02
+			jmp s_end
+				ptnt_s:		
+				add word[score],10
+				
+				 mov ax,[score]
+				 push ax
+				 call printnum
+		      call beep				
+				mov byte[row_index],ch
+				mov byte[col_index],cl
+				sub byte[col_index],3
+				sub byte[row_index],1
+				mov si,0
+				j2:mov dh, byte[row_index]
+				mov dl, byte[col_index]
+				mov bh, 0
+				mov ah, 2
+				int 10h
+				
+				mov al,'*'
+				mov bh,0
+				mov bl,10001011b
+				mov cx,6
+				mov ah,09h
+				int 10h
+				add byte[row_index],1
+				add si,1
+				cmp si,3
+				jne j2
+				
+				
+				s_end:
+				ret
+				
+				
+		cmp_g:
+		
+		mov ch,0  ;row
+			l03:mov cl,20 ;col
+				l4:mov dh, ch
+					mov dl, cl
+					mov bh, 0
+					mov ah, 2
+					int 10h
+					mov bh,0
+					mov ah,08h
+					int 10h
+					cmp al,'g'
+					je ptnt_g
+					add cl,1
+					cmp cl,78
+					jne l4
+				add ch,1
+				cmp ch,24
+				jne l03
+				jmp g_end
+						ptnt_g:		
+				add word[score],10
+				
+				 mov ax,[score]
+				 push ax
+				 call printnum
+			     call beep
+				mov byte[row_index],ch
+				mov byte[col_index],cl
+				sub byte[col_index],3
+				sub byte[row_index],1
+				mov si,0
+				j3:mov dh, byte[row_index]
+				mov dl, byte[col_index]
+				mov bh, 0
+				mov ah, 2
+				int 10h
+				
+				mov al,'*'
+				mov bh,0
+				mov bl,10001110b
+				mov cx,6
+				mov ah,09h
+				int 10h
+				add byte[row_index],1
+				add si,1
+				cmp si,3
+				jne j3
+				
+				
+				g_end:
+				ret
+		
 		cmp_k:
 		
 		mov ch,0  ;row
@@ -644,7 +614,8 @@ scrollup: push ax
 				
 				k_end:
 				ret
-cmp_z:
+		
+		cmp_z:
 		
 		mov ch,0  ;row
 			l05:mov cl,20 ;col
@@ -663,7 +634,8 @@ cmp_z:
 					jne l6
 				add ch,1
 				cmp ch,24
-				jne l05	
+				jne l05
+		
 		jmp z_end
 						ptnt_z:		
 				add word[score],10
@@ -697,7 +669,7 @@ cmp_z:
 				
 				z_end:
 				ret
-	cmp_l:
+		cmp_l:
 		mov ch,0  ;row
 			l06:mov cl,20 ;col
 				l7:mov dh, ch
@@ -749,6 +721,60 @@ cmp_z:
 				
 				l_end:
 				ret
+		
+		cmp_p:
+		mov ch,0  ;row
+			l07:mov cl,20 ;col
+				l8:mov dh, ch
+					mov dl, cl
+					mov bh, 0
+					mov ah, 2
+					int 10h
+					mov bh,0
+					mov ah,08h
+					int 10h
+					cmp al,'p'
+					je ptnt_p
+					add cl,1
+					cmp cl,78
+					jne l8
+				add ch,1
+				cmp ch,24
+				jne l07
+		jmp p_end
+						ptnt_p:		
+				add word[score],10
+				
+				 mov ax,[score]
+				 push ax
+				 call printnum
+				call beep
+				mov byte[row_index],ch
+				mov byte[col_index],cl
+				sub byte[col_index],3
+				sub byte[row_index],1
+				mov si,0
+				j7:mov dh, byte[row_index]
+				mov dl, byte[col_index]
+				mov bh, 0
+				mov ah, 2
+				int 10h
+				
+				mov al,'*'
+				mov bh,0
+				mov bl,10001011b
+				mov cx,6
+				mov ah,09h
+				int 10h
+				add byte[row_index],1
+				add si,1
+				cmp si,3
+				jne j7
+				
+				
+				p_end:
+				ret
+				
 		compare:
 		mov ah,00h
 		int 16h
@@ -801,7 +827,9 @@ cmp_z:
 		
 		end:
 		ret
-	printrec:
+		
+		
+		printrec:
 		push bp
 		mov bp,sp
 		sub sp,2
@@ -864,7 +892,10 @@ cmp_z:
 		mov sp,bp
 		pop bp
 		ret 6
-			print_sc:
+		
+		
+		
+	print_sc:
 	push ax
 	push cx
 		;1st
@@ -895,6 +926,7 @@ cmp_z:
 		push ax
 		mov byte[char],'s'
 		call printrec	
+		
 		;3rth
 		mov byte[top_col],70
 		mov byte[top_row],20
@@ -921,7 +953,7 @@ cmp_z:
 		push ax
 		mov byte[char],'k'
 		call printrec	
-;5th
+		;5th
 		mov byte[top_col],53
 		mov byte[top_row],7	
 		mov byte[bot_col],58
@@ -947,7 +979,7 @@ cmp_z:
 		push ax
 		mov byte[char],'l'
 		call printrec
-;7th
+		;7th
 		mov byte[top_col],60
 		mov byte[top_row],7	
 		mov byte[bot_col],65
@@ -962,8 +994,9 @@ cmp_z:
 		call printrec
 		pop cx
 		pop ax
-ret
-main_print:;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+		ret
+		
+		main_print:;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 		push cx
 		mov cx,4   ;2 loop take 68 sec
 		lop1:call print_sc
@@ -973,6 +1006,8 @@ main_print:;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 		pp_end:
 		pop cx
 		ret
+
+
 ;;;;;;;;;;;;;;;;time ;;;;;;;;;;;;;;;;;
 		timer:
 		;call update_time
@@ -983,6 +1018,8 @@ main_print:;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 		MOV     AH, 86H
 		INT     15H
 		ret
+		
+		
 		printstr: push bp
 		 mov bp, sp
 		 push es
@@ -1000,7 +1037,7 @@ main_print:;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 		 mov si, [bp+6] ; point si to string
 		 mov cx, [bp+4] ; load length of string in cx
 		 mov ah, [bp+8] ; load attribute in ah
-		 nextchar1: mov al, [si] ; load next char of string
+		nextchar1: mov al, [si] ; load next char of string
 		 mov [es:di], ax ; show this char on screen
 		 add di, 2 ; move to next screen location
 		 add si, 1 ; move to next char in string
@@ -1012,6 +1049,7 @@ main_print:;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 		 pop es
 		 pop bp
 		 ret 10 
+ 
  print_t_s:
  push ax
  push bx
@@ -1030,7 +1068,18 @@ main_print:;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 		 add bx,1
 		 cmp bx,18
 		 jne loop1
-	 
+		 
+		  mov ax,4
+		  push ax ; push x position
+		 mov ax, 6
+		 push ax ; push y position
+		 mov ax, 00001011b ; blue on black attribute
+		 push ax ; push attribute
+		 mov ax, msg
+		 push ax ; push address of message
+		push  word [len];; push message length
+		 call printstr
+		 
 		 mov bx,3
 	loop2:	 mov ax, bx
 		 push ax ; push x position
@@ -1045,6 +1094,8 @@ main_print:;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 		 add bx,1
 		 cmp bx,18
 		 jne loop2
+		 
+		 
 		 mov bx,3
 	loop11:	 mov ax, bx
 		 push ax ; push x position
@@ -1074,7 +1125,8 @@ main_print:;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 		 mov ax,[score]
 		 push ax
 		 call printnum
- mov bx,3
+		 
+		 mov bx,3
 	loop22:	 mov ax, bx
 		 push ax ; push x position
 		 mov ax, 16
@@ -1100,9 +1152,14 @@ main_print:;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 		;call update_time
 		pop bx
 		 pop ax
+		ret
+		
+		
+		
+		
 
-	ret
-game_ending:
+		
+	game_ending:
 		 push ax
 		 mov ax, 10
 		 push ax ; push x position
@@ -1114,7 +1171,7 @@ game_ending:
 		 push ax ; push address of message
 		push  word [len_over];; push message length
 		 call printstr 
-
+		 
 		  mov ax, 10
 		 push ax ; push x position
 		 mov ax, 5
@@ -1125,8 +1182,8 @@ game_ending:
 		 push ax ; push address of message
 		push  word [len_over];; push message length
 		 call printstr
-
-				  mov ax, 10
+		 
+		  mov ax, 10
 		 push ax ; push x position
 		 mov ax, 6
 		 push ax ; push y position
@@ -1136,7 +1193,8 @@ game_ending:
 		 push ax ; push address of message
 		push  word [len_over];; push message length
 		 call printstr
-			  mov ax, 10
+		 
+		  mov ax, 10
 		 push ax ; push x position
 		 mov ax, 7
 		 push ax ; push y position
@@ -1146,8 +1204,8 @@ game_ending:
 		 push ax ; push address of message
 		push  word [len_over];; push message length
 		 call printstr
-
-				  mov ax, 10
+		 
+		  mov ax, 10
 		 push ax ; push x position
 		 mov ax, 8
 		 push ax ; push y position
@@ -1157,7 +1215,8 @@ game_ending:
 		 push ax ; push address of message
 		push  word [len_over];; push message length
 		 call printstr
-			  mov ax, 10
+		 
+		  mov ax, 10
 		 push ax ; push x position
 		 mov ax, 9
 		 push ax ; push y position
@@ -1167,6 +1226,7 @@ game_ending:
 		 push ax ; push address of message
 		push  word [len_over];; push message length
 		 call printstr
+		 
 		  mov ax, 10
 		 push ax ; push x position
 		 mov ax, 10
@@ -1177,8 +1237,8 @@ game_ending:
 		 push ax ; push address of message
 		push  word [len_over];; push message length
 		 call printstr
-
-				 mov ax, 10
+		
+		 mov ax, 10
 		 push ax ; push x position
 		 mov ax, 11
 		 push ax ; push y position
@@ -1199,30 +1259,8 @@ game_ending:
 		 push ax ; push address of message
 		push  word [len_over];; push message length
 		 call printstr
-
-				 mov ax, 10
-		 push ax ; push x position
-		 mov ax, 11
-		 push ax ; push y position
-		 mov ax,  00111111b; blue on black attribute
-		 push ax ; push attribute
-		 mov ax, e8
-		 push ax ; push address of message
-		push  word [len_over];; push message length
-		 call printstr
 		 
 		  mov ax, 10
-		 push ax ; push x position
-		 mov ax, 12
-		 push ax ; push y position
-		 mov ax,  00111111b ; blue on black attribute
-		 push ax ; push attribute
-		 mov ax, e9
-		 push ax ; push address of message
-		push  word [len_over];; push message length
-		 call printstr
-
-				  mov ax, 10
 		 push ax ; push x position
 		 mov ax, 13
 		 push ax ; push y position
@@ -1242,9 +1280,9 @@ game_ending:
 		 mov ax, e11
 		 push ax ; push address of message
 		push  word [len_over];; push message length
-		 call printstr 
-
-				 mov ax, 10
+		 call printstr
+		 
+		 mov ax, 10
 		 push ax ; push x position
 		 mov ax, 15
 		 push ax ; push y position
@@ -1265,7 +1303,8 @@ game_ending:
 		 push ax ; push address of message
 		push  word [len_over];; push message length
 		 call printstr
-			   mov ax, 10
+		 
+		   mov ax, 10
 		 push ax ; push x position
 		 mov ax, 17
 		 push ax ; push y position
@@ -1284,9 +1323,9 @@ game_ending:
 		 mov ax, e15
 		 push ax ; push address of message
 		push  word [len_over];; push message length
-		 call printstr	 
-
-				  mov ax, 20
+		 call printstr
+		 
+		  mov ax, 20
 		 push ax ; push x position
 		 mov ax, 17
 		 push ax ; push y position
@@ -1301,13 +1340,16 @@ game_ending:
 		 push ax
 		 call printnum2
 		pop ax
-		ret
+		ret		
+		
 start:
-	call clrscr
+
+
+       call clrscr
 		call timer
 		call beep
 		call clrscr
 	call game_ending
 	
     mov ax,0x4c00
-int 0x21
+    int 0x21
