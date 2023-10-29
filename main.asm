9e37a97c68182cc5bc68e77f060f92408a71f780
Hafsa Najeeb <115767193+Hafsa9876@users.noreply.github.com>
2023-10-29 18:13:58 +0500

    Update main.asm

--- main.asm
+++ main.asm
@@ -1146,6 +1146,17 @@ game_ending:
 		 push ax ; push address of message
 		push  word [len_over];; push message length
 		 call printstr
+
+				  mov ax, 10
+		 push ax ; push x position
+		 mov ax, 8
+		 push ax ; push y position
+		 mov ax,  00111111b; blue on black attribute
+		 push ax ; push attribute
+		 mov ax, e5
+		 push ax ; push address of message
+		push  word [len_over];; push message length
+		 call printstr
 		ret
 start:
 	call clrscr
